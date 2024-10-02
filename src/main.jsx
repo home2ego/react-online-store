1d419cb6f1ede8b6df948f9adb5aba04487d032e
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-02 22:12:24 +0200

    add StoreFront Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
+import StoreFront from './StoreFront';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -7,6 +8,7 @@ function App() {
   if (loggedIn) {
     return (
       <>
+        <StoreFront />
         <button className="btn btn-outline" onClick={() => setLoggedIn(false)}>
           Logout
         </button>
